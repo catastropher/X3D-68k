f6b6337fd003c67d99adf2aee1c251b34fc3295f
Michael Wilder <mwilder.w@gmail.com>
2015-02-27 09:57:14 -0500

    Worded on getting polygon clipping to work

--- qbuffer.c
+++ qbuffer.c
@@ -17,6 +17,7 @@ typedef struct {
 	long slope;
 	short b;
 	char sign;
+	char start, end;
 } Line2D;
 
 #define MAX_POINTS 20
@@ -42,13 +43,20 @@ inline short eval_line(Line2D* line, short x) {
 	return (((long)line->slope * x) >> 8) + line->b;
 }
 
-char point_valid_side(Line2D* line, Vex2D* point) {
+char point_valid_side(Line2D* line, Vex2D* point) {	
+	char sign;
+	
 	if(line->slope == VERTICAL_LINE) {
-		return signof(point->x - line->b) == line->sign;
+		sign = signof(point->x - line->b);
 	}
 	else {
-		return signof(point->y - eval_line(line, point->x)) == line->sign;
+		sign = signof(point->y - eval_line(line, point->x));
 	}
+	
+	if(sign == 0)
+		return 0;
+	
+	return sign == line->sign ? 1 : -1;
 }
 
 inline void get_line_info(Line2D* dest, Vex2D* start, Vex2D* end, Vex2D* center) {
@@ -59,11 +67,15 @@ inline void get_line_info(Line2D* dest, Vex2D* start, Vex2D* end, Vex2D* center)
 		dest->slope = VERTICAL_LINE;
 		dest->b = start->x;
 		dest->sign = signof(center->x - start->x);
+		dest->start = min(start->y, end->y);
+		dest->end = max(start->y, end->y);
 	}
 	else {
 		dest->slope = ((long)dy << 8) / dx;
 		dest->b = start->y - (((long)dest->slope * start->x) >> 8);
 		dest->sign = signof(center->y - eval_line(dest, center->x));
+		dest->start = min(start->x, end->x);
+		dest->end = max(start->x, end->x);
 	}
 }
 
@@ -117,48 +129,20 @@ char line2d_intersect(Line2D* a, Line2D* b, Vex2D* res, Vex2D* center)
 	if(a->slope == VERTICAL_LINE) {
 		res->x = a->b;
 		res->y = eval_line(b, a->b);
+		
+		return 1;
+		return res->y >= a->start && res->y <= a->end;
 	}
 	else {
 		res->x = (((long)b->b - a->b) << 8) / (a->slope - b->slope);
-		res->y = eval_line(a, res->x); 
+		res->y = eval_line(a, res->x);
+		
+		return 1;
+		return res->x >= a->start && res->x <= a->end;
 	}
 	
-	return 1;
 	
 	
-	
-#if 0
-	Vex2D dx, dy, d;
-	vsub(x1, x0, &dx);
-	vsub(y1, y0, &dy);
-	vsub(x0, y0, &d);
-	/* x0 + a dx = y0 + b dy ->
-	   x0 X dx = y0 X dx + b dy X dx ->
-	   b = (x0 - y0) X dx / (dy X dx) */
-	int shift = 25;
-	long dyx = cross(&dy, &dx);
-	if (!dyx) return 0;
-	//dyx = (((long)1 << shift) * cross(&d, &dx)) / dyx;
-	
-	long top = ((long)1 << shift) / dyx;
-	
-	top >>= (shift - 16 + 1);
-	
-	if(top > 32768)
-		error("wrong");
-	
-	dyx = top * cross(&d, &dx);
-	
-	
-	
-	if (dyx <= 0 || dyx >= ((long)32768)) return 0;
- 
- 	long dyx_small = dyx;//(short)dyx;
- 
-	res->x = y0->x + (((long)dyx_small * dy.x) >> 15);
-	res->y = y0->y + (((long)dyx_small * dy.y) >> 15);
-	return 1;
-#endif
 }
  
 /* === polygon stuff === */
@@ -202,77 +186,77 @@ int poly_winding(poly p)
 	return left_of(p->v, p->v + 1, p->v + 2);
 }
  
-#if 0
-void poly_edge_clip(Poly* sub, Vex2D* x0, Vex2D* x1, int left, Poly* res, Vex2D* center)
-{
-	int i, side0, side1;
-	Vex2D tmp;
-	Vex2D* v0 = sub->v + sub->len - 1, *v1;
-	res->len = 0;
- 
-	side0 = left_of(x0, x1, v0);
-	if (side0 != -left) poly_append(res, v0);
- 
-	for (i = 0; i < sub->len; i++) {
-		v1 = sub->v + i;
-		side1 = left_of(x0, x1, v1);
-		if (side0 + side1 == 0 && side0)
-			/* last point and current straddle the edge */
-			if (line_sect(x0, x1, v0, v1, &tmp, center))
-				poly_append(res, &tmp);
-		if (i == sub->len - 1) break;
-		if (side1 != -left) poly_append(res, v1);
-		v0 = v1;
-		side0 = side1;
-	}
-}
-#endif
 
 void polygon_clip_edge(Polygon* p, Line2D* edge, Polygon* dest, Vex2D* center) {
 	int i;
+	short prev = p->total_points - 1;
 	Vex2D clip_pos;
 	
 	dest->total_points = 0;
 	
-	for(i = 1; i < p->total_points; i++) {
-		Line2D* line = &p->line[i - 1];
+	short out[20];
+	short out_pos = 0;
+	char last_side = point_valid_side(edge, &p->p[prev].v);
+	Line first;
+	
+	if(last_side != -1) {
+		dest->p[dest->total_points] = p->p[prev];
+		first = p->line[prev];
+	}
+	
+	//printf("Slope: %ld, b: %d\n, sign: %d\n", edge->slope, edge->b, edge->sign);
+	//ngetchx();
+	
+	for(i = 0; i < p->total_points; i++) {
+		Line2D* line = &p->line[prev];
+		char side = point_valid_side(edge, &p->p[i].v);
 		
-		if(!point_valid_side(edge, &p->p[i].v)) {
+		//printf("Slope: %ld, b: %d\n, sign: %d\n", line->slope, line->b, line->sign);
+		//ngetchx();
+		
+		if(side + last_side == 0 && side) {
+			//printf("Outside\n");
+			
 			if(line2d_intersect(line, edge, &clip_pos, center)) {
 				// Add the new clipped point
+				out[out_pos++] = dest->total_points;
+				
 				dest->p[dest->total_points] = (Point){1, clip_pos};
-				// FIXME: dest->line[dest->total_points++] = edge;
+				
+				
+				dest->line[dest->total_points++] = *line;
 			}
 		}
-		else {
+		
+		if(i != p->total_points - 1 && side != -1) {
 			dest->p[dest->total_points] = p->p[i];
 			dest->line[dest->total_points++] = *line;
 		}
+		
+		prev = i;
+		last_side = side;
 	}
+	
+	if(out_pos != 0) {
+		return;
+		
+		short min_p = min(out[0], out[1]);
+		short max_p = max(out[0], out[1]);
+		
+		if(min_p == 0 && max_p == p->total_points - 1) {
+			dest->line[p->total_points - 1] = *edge;
+		}
+		else {
+			dest->line[min_p] = *edge;
+		}
+	}
+	
+	
+	//ngetchx();
 }
 	
+void print_polygon(Polygon* p);
 	
-	
-	
-#if 0
-	side0 = left_of(x0, x1, v0);
-	if (side0 != -left) poly_append(res, v0);
- 
-	for (i = 0; i < sub->len; i++) {
-		v1 = sub->v + i;
-		side1 = left_of(x0, x1, v1);
-		if (side0 + side1 == 0 && side0)
-			/* last point and current straddle the edge */
-			if (line_sect(x0, x1, v0, v1, &tmp, center))
-				poly_append(res, &tmp);
-		if (i == sub->len - 1) break;
-		if (side1 != -left) poly_append(res, v1);
-		v0 = v1;
-		side0 = side1;
-	}
-}
-#endif
-
  
 Polygon* clip_polygon(Polygon* p, Polygon* clip, Polygon* temp_a, Polygon* temp_b)
 {
@@ -281,18 +265,13 @@ Polygon* clip_polygon(Polygon* p, Polygon* clip, Polygon* temp_a, Polygon* temp_
 	Polygon *p1 = temp_a;
 	Polygon *p2 = temp_b;
 	
-	for(i = 0; i < clip->total_points; i++) {
-		center.x += clip->p[i].v.x;
-		center.y += clip->p[i].v.y;
-	}
-	
-	center.x /= clip->total_points;
-	center.y /= clip->total_points;
-	
 	// Clip against the first edge
-	polygon_clip_edge(p, &p->line[clip->total_points - 1], p2, &center);
+	polygon_clip_edge(p, &clip->line[clip->total_points - 1], p2, &center);
+	
+	//return p2;
 	
 	for(i = 0; i < clip->total_points - 1; i++) {
+		print_polygon(p2);
 		SWAP(p1, p2);
 		
 		if(p1->total_points == 0) {
@@ -300,48 +279,40 @@ Polygon* clip_polygon(Polygon* p, Polygon* clip, Polygon* temp_a, Polygon* temp_
 			break;
 		}
 		
-		polygon_clip_edge(p1, &p->line[i], p2, &center);
+		polygon_clip_edge(p1, &clip->line[i], p2, &center);
 	}
 	
 	return p2;
 }
 	
-# if 0
-	poly p1 = temp_a, p2 = temp_b, tmp;
- 
-	int dir = poly_winding(clip);
-	poly_edge_clip(sub, clip->v + clip->len - 1, clip->v, dir, p2, &center);
-	for (i = 0; i < clip->len - 1; i++) {
-		tmp = p2; p2 = p1; p1 = tmp;
-		if(p1->len == 0) {
-			p2->len = 0;
-			break;
-		}
-		poly_edge_clip(p1, clip->v + i, clip->v + i + 1, dir, p2, &center);
-	}
- 
-	return p2;
-}
-#endif
-//==============================================================================================
-
 
 void make_polygon(Vex2D* v, int points, Polygon* p) {
 	p->total_points = points;
 	int i;
+	Vex2D center = {0, 0};
 	
 	for(i = 0; i < points; i++) {
+		center.x += v[i].x;
+		center.y += v[i].y;
+	}
+	
+	center.x /= points;
+	center.y /= points;
+	
+	for(i = 0; i < points; i++) {
+		short next = (i + 1) % points;
 		p->p[i].v = v[i];
+		get_line_info(&p->line[i], &v[i], &v[next], &center); 
 	}
 }
 
 void draw_polygon(Polygon* p) {	
 	int i;
 	for(i = 0; i < p->total_points - 1; i++) {
-		DrawLine(p->p[i].v.x, p->p[i].v.y, p->p[i + 1].v.x, p->p[i + 1].v.y, A_NORMAL); 
+		draw_clip_line(p->p[i].v.x, p->p[i].v.y, p->p[i + 1].v.x, p->p[i + 1].v.y, LCD_MEM); 
 	}
 	
-	DrawLine(p->p[0].v.x, p->p[0].v.y, p->p[p->total_points - 1].v.x, p->p[p->total_points - 1].v.y, A_NORMAL);
+	draw_clip_line(p->p[0].v.x, p->p[0].v.y, p->p[p->total_points - 1].v.x, p->p[p->total_points - 1].v.y, LCD_MEM);
 }
 
 /*
@@ -356,6 +327,20 @@ poly clip_points(Poly* clip, Vex2D* v, int points) {
 
 Vex2D rand_point() {
 	return (Vex2D){rand() % 240, rand() % 128};
+}
+
+void print_polygon(Polygon* p) {
+	int i;
+	
+	printf("===========\n");
+	
+	for(i = 0; i < p->total_points; i++) {
+		Line2D* line = &p->line[i];
+		printf("s: %ld, i: %d, s: %d\n", line->slope, line->b, line->sign); 
+	}
+	
+	printf("===========\n");
+	ngetchx();
 }
 
 void test_polygon_clipper() {
@@ -419,6 +404,8 @@ void test_polygon_clipper() {
 		}
 	};
 	
+	
+	
 	Polygon temp_a, temp_b;
 	
 	Polygon pp;
@@ -427,12 +414,60 @@ void test_polygon_clipper() {
 	Polygon clipp;
 	make_polygon(clip, 4, &clipp);
 	
+	short p_pos = 0;
+	
+	
+	int cx = 39, cy = 64;
+	unsigned short key;
+	
+	draw_polygon(&clipp);
+	
+	do {
+		key = ngetchx();
+		
+		DrawPix(cx, cy, A_REVERSE);
+		
+		if(key == KEY_UP)
+			cy -= 3;
+		else if(key == KEY_DOWN)
+			cy += 3;
+		else if(key == KEY_LEFT)
+			cx -= 3;
+		else if(key == KEY_RIGHT)
+			cx += 3;
+		else if(key == KEY_ENTER) {
+			p[p_pos++] = (Vex2D){cx, cy};
+			
+			if(p_pos != 1)
+				DrawLine(cx, cy, p[p_pos - 2].x, p[p_pos - 2].y, A_NORMAL);
+		}
+		
+		DrawPix(cx, cy, A_NORMAL);
+	} while(key != KEY_ESC);
+	
+	make_polygon(p, p_pos, &pp);
+	
+	//for(i = 0; i < p_pos; i++) {
+	//	Line2D* line = &pp.line[i];
+//		printf("Slope: %ld, b: %d, sign: %d\n", line->slope, line->b, line->sign);
+//	}
+
+#if 0
+	for(i = 0; i < clipp.total_points; i++) {
+		Line2D* line = &clipp.line[i];
+		printf("Slope: %ld, b: %d, sign: %d\n", line->slope, line->b, line->sign);
+	}
+	
+	ngetchx();
+#endif
+	
+	
 	draw_polygon(&pp);
 	ngetchx();
 	draw_polygon(&clipp);
 	ngetchx();
 	
-	Polygon* res = clip_polygon(&clipp, &pp, &temp_a, &temp_b);
+	Polygon* res = clip_polygon(&pp, &clipp, &temp_a, &temp_b);
 	
 	clrscr();
 	draw_polygon(&clipp);
@@ -442,11 +477,11 @@ void test_polygon_clipper() {
 	do {
 		clrscr();
 		draw_polygon(res);
+		draw_polygon(&clipp);
 		ngetchx();
 		
 		clrscr();
 		draw_polygon(&pp);
-		ngetchx();
 		draw_polygon(&clipp);
 	} while(ngetchx() != KEY_ESC);
 }
