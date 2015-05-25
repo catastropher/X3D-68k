62c6ab64382704c9d63d10471d7a6d131e931c8a
Michael Wilder <mwilder.w@gmail.com>
2015-05-25 01:32:08 -0400

    Added fixed-point arithmetic header

---
+++ src/headers/fix.h
@@ -0,0 +1,17 @@
+/// An 8-bit signed integer
+typedef char int8;
+
+/// An 8-bit unsigned integer
+typedef unsigned char uint8;
+
+/// A 16-bit signed integer
+typedef short int16;
+
+/// A 16-bit unsigned integer
+typedef unsigned unsigned short uint16;
+
+/// A 32-bit signed integer
+typedef long int32;
+
+/// A 32-bit unsigned integer
+typedef unsigned long uint32;
