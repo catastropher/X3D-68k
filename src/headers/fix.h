2203da1d346b99ed0ce03308a4f2d98c26278bfd
Jason McGough <j.mcgough227@gmail.com>
2015-05-25 16:29:20 -0400

    Added fixed point typedefs and fixed point add function

--- src/headers/fix.h
+++ src/headers/fix.h
@@ -15,3 +15,22 @@ typedef long int32;
 
 /// A 32-bit unsigned integer
 typedef unsigned long uint32;
+
+/// An 8.8 fixed point number
+typedef int16 fp8x8;
+
+/// A 16.16 fixed point number
+typedef int32 fp16x16;
+
+/// A 0.16 fixed point number
+typedef int16 fp0x16;
+
+
+static inline fp8x8 x3d_add_fp8x8(fp8x8 a, fp8x8 b) {
+#if X3D_CHECK_OVERFLOW
+  x3d_errorif(a + b != (long)a + b, "%d, %d", a, b);
+#endif
+
+  return a + b;
+}
+
