a5f888e6c2ce23a7c850744ddd1fd04ea8b9b7b0
Michael Wilder <mwilder.w@gmail.com>
2015-05-28 18:53:46 -0400

    Worked on vector functions

--- src/source/vector.c
+++ src/source/vector.c
@@ -18,22 +18,45 @@
 #include "X3D_error.h"
 
 /**
-* Calculates the short dot product of two 3D vectors.
+* Calculates the dot product of two 16-bit integer 3D vectors.
 *
 * @param a - pointer to the first 3D vector
 * @param b - pointer to the the second 3D vector
 *
-* @return dot product of a and b as a short
-*
-* @note This returns the dot product shifted left by @ref NORMAL_BITS to fit in
-*   a short.
+* @return dot product of a and b as an int32
 */
-int16 x3d_dot_product(Vex3D* a, Vex3D* b) {
-  return ((int32)a->x * b->x + (int32)a->y * b->y + (int32)a->z * b->z) >> NORMAL_BITS;
+int32 x3d_vex3d_int16_dot(X3D_Vex3D_int16* a, X3D_Vex3D_int16* b) {
+#ifdef X3D_CHECK_OVERFLOW
+
+  int32 x = (int32)a->x * b->y;
+  int32 y = (int32)a->y * b->y;
+  int32 z = (int32)a->z * b->z;
+
+  int32 xplusy, result;
+
+  if(!__builtin_saddl_overflow(x, y, &xplusy)) {
+    if(!__builtin_saddl_overflow(xplusy, z, &result)) {
+      return result;
+    }
+  }
+
+  x3d_error("int16 dot product overflow\na: {%d, %d, %d}\nb: {%d, %d, %d}",
+    a->x, a->y, a->z, b->x, b->y, b->z);
+
+#else
+
+  return a->x * b->x + (int32)a->y * b->y + (int32)a->z * b->z;
+
+#endif
+}
+
+// @todo comment
+void x3d_print_vex3d_int16(X3D_Vex3D_int16* v) {
+  printf("{%d, %d, %d}\n", v->x, v->y, v->z);
 }
 
 /**
-* Calculates the dot product of two 3D vectors.
+* Calculates the dot product of two 0x16 fixed-point 3D vectors.
 *
 * @param a - pointer to the first 3D vector
 * @param b - pointer to the second 3D vector
@@ -42,9 +65,11 @@ int16 x3d_dot_product(Vex3D* a, Vex3D* b) {
 *
 * @note This returns the unshifted dot product.
 */
-int32 x3d_dot_product_long(Vex3D* a, Vex3D* b) {
-  return (int32)a->x * b->x + (int32)a->y * b->y + (int32)a->z * b->z;
+int16 x3d_vex3d_fp0x16_dot(X3D_Vex3D_fp0x16* a, X3D_Vex3D_fp0x16* b) {
+  return x3d_vex3d_int16_dot(a, b) >> X3D_NORMAL_BITS;
 }
+
+#if 0
 
 /**
 * Normalizes a 3D vector (makes the entire length 1). The result is in 0.15
@@ -76,4 +101,4 @@ inline void x3d_normalize_vex3d(Vex3D* v) {
   v->z = ((int32)v->z << NORMAL_BITS) / len;
 }
 
-
+#endif
