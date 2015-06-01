9eb3223c4eaa594b5417543e0dea14e04c00153d
Michael Wilder <mwilder.w@gmail.com>
2015-06-01 14:27:22 -0400

    Began sketching out rendering structures

--- src/source/segment.c
+++ src/source/segment.c
@@ -17,38 +17,29 @@
 #include "X3D_fix.h"
 
 typedef struct {
-  int16 size;
-  uint8* ptr;
-  uint8* base;
-} X3D_Stack;
 
-typedef struct X3D_RenderContext {
-  uint8 flags;
-  uint8 fov;
-  int16 x, y;
-  int16 w, h;
+} X3D_EdgeClip;
+
+typedef struct {
+  uint8 total_v;
   
-  X3D_Stack stack;    /// Render stack
+  //uint16 v[];           // Vertex ID's that make up the face
+  uint32 edge_bits;     // For edge propogration
+} X3D_Face;
   
-} X3D_RenderContext;
 
 
-static inline void* x3d_stack_alloc(X3D_Stack* stack, uint16 bytes) {
-  stack->ptr -= bytes;
-  return stack->ptr;
-}
-
-void* x3d_stack_save(X3D_Stack* stack) {
-  return stack->ptr;
-}
-
-static inline void x3d_stack_restore(X3D_Stack* stack, void* ptr) {
-  stack->ptr = ptr;
-}
-
-static inline void x3d_stack_create(X3D_Stack* stack, uint16 size) {
-  /// @TODO: replace with x3d_malloc
-  stack->base = malloc(size);
-  stack->size = size;
-  stack->ptr = stack->base + size;
-}
+typedef struct {
+  uint8 total_e;        // Total number of edges
+  uint8 total_v;         // Total number of veritices
+  uint8 total_f;        // Total number of faces
+  
+  uint16 engine_delta;  // Last delta the segment was rendered
+  uint32 drawn_edges;    // Which edges have already been drawn
+  
+  X3D_EdgeClip* clipped_edges[];
+} X3D_Segment;
+  
+  
+  
+  
