c35bc93c19e9d9a95514b29457c647b453008a07
Michael Wilder <mwilder.w@gmail.com>
2015-06-11 12:18:27 -0400

    Updated integer typedefs to use stdint.h types

--- src/headers/X3D_fix.h
+++ src/headers/X3D_fix.h
@@ -4,29 +4,31 @@
 #include "X3D_config.h"
 #include "x3d_error.h"
 
+#include <stdint.h>
+
 /// An 8-bit signed integer
-typedef char int8;
+typedef int8_t int8;
 
 /// An 8-bit unsigned integer
-typedef unsigned char uint8;
+typedef uint8_t uint8;
 
 /// A 16-bit signed integer
-typedef short int16;
+typedef int16_t int16;
 
 /// A 16-bit unsigned integer
-typedef unsigned short uint16;
+typedef uint16_t uint16;
 
 /// A 32-bit signed integer
-typedef long int32;
+typedef int32_t int32;
 
 /// A 32-bit unsigned integer
-typedef unsigned long uint32;
+typedef uint32_t uint32;
 
 /// A 64-bit signed integer
-typedef long long int64;
+typedef int64_t int64;
 
 /// A 64-bit unsigned integer
-typedef unsigned long long uint64;
+typedef uint16_t uint64;
 
 /// An 8.8 fixed point number
 typedef int16 fp8x8;
