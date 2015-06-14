8bcb7f92945dac1b9be60fae0e5676d9a39a6b36
Michael Wilder <mwilder.w@gmail.com>
2015-06-14 17:44:34 -0400

    Reorganized X3D_segment.h

--- src/headers/X3D_segment.h
+++ src/headers/X3D_segment.h
@@ -17,49 +17,23 @@
 #include "X3D_fix.h"
 #include "X3D_vector.h"
 
+//=============================================================================
+// Forward declarations
+//=============================================================================
+struct X3D_RenderContext;
+
+//=============================================================================
+// Structures
+//=============================================================================
 
 typedef struct {
   uint16 base_v;     // Number of vertices in a base
   X3D_Vex3D_int16 v[0];
 } X3D_Prism;
 
-struct X3D_RenderContext;
-
+//=============================================================================
+// Function declarations
+//=============================================================================
 void x3d_prism_construct(X3D_Prism* s, uint16 steps, uint16 r, int16 h, X3D_Vex3D_angle256 rot_angle);
 void x3d_prism_render(X3D_Prism* prism, struct X3D_RenderContext* context);
-
-#if 0
-
-typedef struct {
-
-} X3D_EdgeClip;
-
-typedef struct {
-  uint8 from_edge, to_edge;
-} X3D_ConnectEdge;
-
-typedef struct {
-  uint8 total_v;
-  
-  uint32 edge_bits;     // For edge propogration
-
-  uint16 v[0];           // Vertex ID's that make up the face
-  X3D_ConnectEdge connect_edge[0];
-
-} X3D_Face;
-  
-
-
-typedef struct {
-  uint8 total_e;        // Total number of edges
-  uint8 total_v;         // Total number of veritices
-  uint8 total_f;        // Total number of faces
-  
-  uint16 engine_delta;  // Last delta the segment was rendered
-  uint32 drawn_edges;    // Which edges have already been drawn
-  
-  X3D_EdgeClip* clipped_edges[];
-} X3D_Segment;
-
-#endif
 
