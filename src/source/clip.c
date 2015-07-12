1892a9a87ac553d1c43b72e96a98c887f129b103
Michael Wilder <mwilder.w@gmail.com>
2015-07-12 14:47:31 -0400

    Worked on new clipping algorithm for solid rendering

--- src/source/clip.c
+++ src/source/clip.c
@@ -21,16 +21,18 @@
 #include "X3D_render.h"
 #include "X3D_frustum.h"
 #include "X3D_error.h"
+#include "X3D_segment.h"
+#include "X3D_prism.h"
 
 
 // Parameterizes the line between two points
 void x3d_param_line2d(X3D_ParamLine2D* line, X3D_Vex2D_int16* a, X3D_Vex2D_int16* b) {
   int16 dx = b->x - a->x;
   int16 dy = b->y - a->y;
-  
+
   // Calculate the normal for the line
-  line->normal = (X3D_Vex2D_int16){ -dy, dx };
-  
+  line->normal = (X3D_Vex2D_int16) { -dy, dx };
+
   // Calculate the distance to the origin
   /// @todo add overflow checking
   line->d = ((int32)line->normal.x * a->x + (int32)line->normal.y * a->y);
@@ -40,24 +42,24 @@ void x3d_param_line2d(X3D_ParamLine2D* line, X3D_Vex2D_int16* a, X3D_Vex2D_int16
 
 X3D_ClipRegion* x3d_construct_clipregion(X3D_Vex2D_int16* v, uint16 total_v) {
   uint16 i;
-  
+
   X3D_ClipRegion* r = malloc(sizeof(X3D_ClipRegion) + sizeof(X3D_ParamLine2D) * total_v);
-  
+
   for(i = 0; i < total_v; ++i) {
     uint16 next = (i + 1) % total_v;
-    
+
     x3d_param_line2d(&r->pl[i], v + i, v + next);
   }
-  
+
   r->total_pl = total_v;
-  
+
   return r;
 }
 
 inline void get_edge(X3D_Prism2D* p, uint16 id, uint16* a, uint16* b) {
   if(id < p->base_v) {
     *a = id;
-    
+
     if(id != p->base_v - 1)
       *b = id + 1;
     else
@@ -65,7 +67,7 @@ inline void get_edge(X3D_Prism2D* p, uint16 id, uint16* a, uint16* b) {
   }
   else if(id < p->base_v * 2) {
     *a = id;
-    
+
     if(id != p->base_v * 2 - 1)
       *b = id + 1;
     else
@@ -85,7 +87,7 @@ void x3d_prism2d_clip(X3D_Prism2D* prism, X3D_ClipRegion* clip, X3D_RenderContex
   // Check each point against each bounding line
   uint16 i, d;
   int32 dist[clip->total_pl][prism->base_v * 2];
-  
+
   for(i = 0; i < clip->total_pl; ++i) {
     for(d = 0; d < prism->base_v * 2; ++d) {
       dist[i][d] = (int32)clip->pl[i].normal.x * prism->v[d].x + (int32)clip->pl[i].normal.y * prism->v[d].y - clip->pl[i].d;
@@ -93,35 +95,35 @@ void x3d_prism2d_clip(X3D_Prism2D* prism, X3D_ClipRegion* clip, X3D_RenderContex
       //ngetchx();
     }
   }
-  
+
   for(i = 0; i < prism->base_v * 3; ++i) {
     uint16 a, b;    // Edge vertices
     uint16 clip_v;
-    
+
     int16 a_min_scale = 0x7FFF;
     int16 b_min_scale = 0x7FFF;
-    
+
     get_edge(prism, i, &a, &b);
-    
+
     _Bool need_clip = 1;
-    
+
     // Check each bounding line to see if a or b is outside
     for(d = 0; d < clip->total_pl; ++d) {
-      
+
       _Bool a_out = dist[d][a] < 0;
       _Bool b_out = dist[d][b] < 0;
-      
+
       if(a_out || b_out) {
         //printf("ERROR OUT\n");
         //ngetchx();
       }
-      
+
       if(a_out != b_out) {
         if(b_out) {
           SWAP(a, b);
           SWAP(a_min_scale, b_min_scale);
         }
-        
+
         int16 scale = ((int32)abs(dist[d][a]) << 8) / (abs(dist[d][b]) + abs(dist[d][a]));
 
         if(scale < a_min_scale) {
@@ -131,7 +133,7 @@ void x3d_prism2d_clip(X3D_Prism2D* prism, X3D_ClipRegion* clip, X3D_RenderContex
       else if(!a_out) {
         //X3D_Vex2D_int16 v_a = prism->v[a];
         //X3D_Vex2D_int16 v_b = prism->v[b];
-        
+
         //x3d_draw_line_black(context, &v_a, &v_b);
         //need_clip = 0;
         //break;
@@ -142,23 +144,23 @@ void x3d_prism2d_clip(X3D_Prism2D* prism, X3D_ClipRegion* clip, X3D_RenderContex
         break;
       }
     }
-    
+
     if(need_clip) {
       X3D_Vex2D_int16 v_a = prism->v[a];
       X3D_Vex2D_int16 v_b = prism->v[b];
-      
+
       if(a_min_scale != 0x7FFF) {
         v_a.x += (((int32)prism->v[b].x - prism->v[a].x) * a_min_scale) >> 8;
         v_a.y += (((int32)prism->v[b].y - prism->v[a].y) * a_min_scale) >> 8;
         //printf("CLIP A\n");
       }
-      
+
       if(b_min_scale != 0x7FFF) {
         v_b.x += (((int32)prism->v[a].x - prism->v[b].x) * b_min_scale) >> 8;
         v_b.y += (((int32)prism->v[a].y - prism->v[b].y) * b_min_scale) >> 8;
         //printf("CLIP B\n");
       }
-      
+
       x3d_draw_line_black(context, &v_a, &v_b);
     }
   }
@@ -243,14 +245,14 @@ inline void x3d_get_prism3d_edge(X3D_Prism3D* p, uint16 id, uint16* a, uint16* b
 
 /**
   Draws a wireframe 3D prism, clipped against a frustum.
-  
+
   @param prism      - prism to draw
   @param frustum    - frustum to clip against
   @param context    - rendering context to draw to
-*/
+  */
 void x3d_draw_clipped_prism3d_wireframe(X3D_Prism* prism, X3D_Frustum* frustum, X3D_RenderContext* context, uint16 select_a, uint16 select_b) {
   //X3D_LOG_WAIT(context, "Planes: %d\n", frustum->total_p);
-  
+
   uint16 i, d, vertex, plane, edge;
   const int TOTAL_V = prism->base_v * 2;
   uint8 was_projected[TOTAL_V];
@@ -278,7 +280,7 @@ void x3d_draw_clipped_prism3d_wireframe(X3D_Prism* prism, X3D_Frustum* frustum, 
 
       if(dist[vertex][plane] < 0) {
         outside[vertex][outside_total[vertex]++] = plane;
-        
+
         //printf("Dist: %d\n", dist[vertex][plane]);
         //X3D_LOG_WAIT(context, "Neg!: %ld\n", dist[vertex][plane]);
       }
@@ -289,7 +291,7 @@ void x3d_draw_clipped_prism3d_wireframe(X3D_Prism* prism, X3D_Frustum* frustum, 
 
   for(edge = 0; edge < prism->base_v * 3 + 1; ++edge) {
     uint16 a, b;
-    
+
     if(edge < prism->base_v * 3)
       x3d_get_prism3d_edge(prism, edge, &a, &b);
     else {
@@ -397,13 +399,221 @@ project_and_draw:
 
 
 
+// Gets a list of edge id's (in order) that make up a segment's face
+uint16 x3d_prism3d_get_edges(X3D_Prism3D* prism, uint16 face, uint16 dest[]) {
+  uint16 i;
+  
+  if(face == BASE_A) {
+    for(i = 0; i < prism->base_v; ++i) {
+      dest[i] = i;
+    }
+
+    return prism->base_v;
+  }
+  else if(face == BASE_B) {
+    for(i = 0; i < prism->base_v; ++i) {
+      dest[i] = i + prism->base_v;
+    }
+
+    return prism->base_v;
+  }
+
+  dest[0] = face - 2;
+  dest[1] = face - 2 + prism->base_v;
+  dest[2] = x3d_single_wrap(face - 2 + 1, prism->base_v) + prism->base_v;
+  dest[3] = x3d_single_wrap(face - 2 + 1, prism->base_v);
+
+  return 4;
+}
 
 
 
 
 
 
+#define EDGE_INVISIBLE 0xFF
+#define EDGE_INSIDE 0
+#define EDGE_CLIP_A 2
+#define EDGE_CLIP_B 1
+#define EDGE_CLIP_BOTH 3
 
 
+//=============================================================================
+/**
+Draws a wireframe 3D prism, clipped against a frustum.
 
+@param prism      - prism to draw
+@param frustum    - frustum to clip against
+@param context    - rendering context to draw to
+*/
+void x3d_draw_clipped_segment_solid(X3D_Segment* seg, X3D_Frustum* frustum, X3D_RenderContext* context, uint16 select_a, uint16 select_b) {
+  //X3D_LOG_WAIT(context, "Planes: %d\n", frustum->total_p);
+  X3D_Prism3D* prism = &seg->prism;
+
+
+  uint16 i, d, vertex, plane, edge, face;
+  const int TOTAL_V = prism->base_v * 2;
+  uint8 was_projected[TOTAL_V];
+  X3D_Vex2D_int16 project[TOTAL_V];
+  X3D_Vex2D_int16 p[2];
+
+  // A list for each vertex that holds which planes it's outside of
+  uint16 outside[TOTAL_V][frustum->total_p];
+  uint16 outside_total[TOTAL_V];
+
+  uint16 outside_mask[TOTAL_V];
+
+  // Calculate the distance between every point and every edge
+  int16 dist[TOTAL_V][frustum->total_p];
+
+  const uint16 total_e = prism->base_v * 3;
+
+  uint8 edge_status[total_e];
+  X3D_Edge clipped_edges[total_e];
+
+
+  for(vertex = 0; vertex < TOTAL_V; ++vertex) {
+    outside_total[vertex] = 0;
+    outside_mask[vertex] = 0;
+    was_projected[vertex] = 0;
+
+    for(plane = 0; plane < frustum->total_p; ++plane) {
+      dist[vertex][plane] = x3d_vex3d_fp0x16_dot(&frustum->p[plane].normal, &prism->v[vertex]) - frustum->p[plane].d;
+
+      outside_mask[vertex] = (outside_mask[vertex] << 1) | (dist[vertex][plane] < 0);
+
+      if(dist[vertex][plane] < 0) {
+        outside[vertex][outside_total[vertex]++] = plane;
+
+        //printf("Dist: %d\n", dist[vertex][plane]);
+        //X3D_LOG_WAIT(context, "Neg!: %ld\n", dist[vertex][plane]);
+      }
+    }
+  }
+
+  //X3D_LOG_WAIT(context, "Dist");
+
+  for(edge = 0; edge < total_e; ++edge) {
+    uint16 a, b;
+
+    if(edge < prism->base_v * 3)
+      x3d_get_prism3d_edge(prism, edge, &a, &b);
+    else {
+      // Draw the selection spinner, if enabled
+      if(select_a != select_b) {
+        a = select_a;
+        b = select_b;
+      }
+      else {
+        break;
+      }
+    }
+
+    // If two edges fail by the same plane, we can skip it
+    if((outside_mask[a] & outside_mask[b]) != 0) {
+      edge_status[i] = EDGE_INVISIBLE;
+      continue;
+    }
+
+    int16 min_scale[2] = { 0x7FFF, 0x7FFF };
+    X3D_Vex3D_int16 clipped[2] = { prism->v[a], prism->v[b] };
+
+    // If both points are inside all planes, the line is totally visible!
+    if((outside_total[a] | outside_total[b]) == 0) {
+      edge_status[i] = EDGE_INSIDE;
+      goto project_and_draw;
+    }
+
+    edge_status[i] = 0;
+
+#if 1
+    for(vertex = 0; vertex < 2; ++vertex) {
+      edge_status[i] = (edge_status[i] << 1) | (outside_total[a] != 0);
+
+      if(outside_total[a] != 0) {
+        int16 min_scale_index = 0;
+
+        for(i = 0; i < outside_total[a]; ++i) {
+          int16 d = abs(dist[b][outside[a][i]]) + abs(dist[a][outside[a][i]]);
+          int16 n = abs(dist[b][outside[a][i]]);
+
+          if(n == d)
+            continue;
+
+          if(d < 1)
+            continue;
+
+          int16 scale = ((int32)n << 8) / (d);
+
+          //printf("Scale: %d\n", scale);
+
+          if(scale < min_scale[vertex]) {
+            min_scale[vertex] = scale;
+            min_scale_index = outside[vertex][i];
+          }
+        }
+
+        if(min_scale[vertex] != 0x7FFF) {
+          clipped[vertex].x = prism->v[b].x + ((((int32)prism->v[a].x - prism->v[b].x) * min_scale[vertex]) >> 8);
+          clipped[vertex].y = prism->v[b].y + ((((int32)prism->v[a].y - prism->v[b].y) * min_scale[vertex]) >> 8);
+          clipped[vertex].z = prism->v[b].z + ((((int32)prism->v[a].z - prism->v[b].z) * min_scale[vertex]) >> 8);
+        }
+      }
+
+      SWAP(a, b);
+    }
+#endif
+
+#if 1
+    // If both of the points were outside, we need to check that the new clip is actually valid
+    if(outside_total[a] != 0 && outside_total[b] != 0) {
+      X3D_Vex3D_int16 mid = { (clipped[0].x + clipped[1].x) >> 1, (clipped[0].y + clipped[1].y) >> 1, (clipped[0].z + clipped[1].z) >> 1 };
+
+      for(plane = 0; plane < frustum->total_p; ++plane) {
+        if(x3d_vex3d_fp0x16_dot(&frustum->p[plane].normal, &mid) - frustum->p[plane].d < 0) {
+          edge_status[i] = EDGE_INVISIBLE;
+          continue;
+        }
+      }
+    }
+#endif
+
+project_and_draw:
+    for(vertex = 0; vertex < 2; ++vertex) {
+
+      if(was_projected[a] && 0) {
+        if(outside_total[a] == 0) {
+          p[vertex] = project[a];
+        }
+      }
+      else {
+        x3d_vex3d_int16_project(&p[vertex], &clipped[vertex], context);
+        x3d_rendercontext_clamp_vex2d_int16(&p[vertex], context);
+
+        if(outside_total[a] == 0) {
+          was_projected[a] = 1;
+          project[a] = p[vertex];
+        }
+      }
+
+      SWAP(a, b);
+    }
+
+    x3d_draw_line_black(context, p, p + 1);
+  }
+
+
+  // Ok, now we have all the clipping data. Now we need to reconstruct the viewing frustum for each face
+  // that has another segment attached to it.
+  const uint16 TOTAL_F = prism->base_v + 2;
+  uint16 edge_list[prism->base_v];
+  uint16 edges;
+
+  for(face = 0; face < TOTAL_F; ++i) {
+    // For right now, we're only going to try this construction with BASE_A
+    if(face == BASE_A) {
+      edges = x3d_prism3d_get_edges(prism, face, edge_list);
+    }
+  }
+}
 
