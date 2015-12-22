ddc599bc25f0c130ffd2c2038552dd10e03d2826
Michael Wilder <mwilder.w@gmail.com>
2015-12-21 22:24:17 -0500

    Documented X3D_config.h (PC port)

--- src/headers/core/engine/X3D_enginestate.h
+++ src/headers/core/engine/X3D_enginestate.h
@@ -1,11 +1,10 @@
 #pragma once
 
-typedef struct X3D_EngineState {
+X3D_INTERNAL typedef struct X3D_EngineState {
   X3D_MemoryManager memory_manager;
 } X3D_EngineState;
 
 X3D_ENGINE_STATE_DECL;
-
 
 X3D_INTERNAL static inline X3D_MemoryManager* x3d_memory_manager_data() {
   return &x3d_engine_state->memory_manager;
