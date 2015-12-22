ea248c9d4e76ba067be0dc356c99dfa75b5952ad
Michael Wilder <mwilder.w@gmail.com>
2015-12-22 14:57:45 -0500

    Util stuff

--- src/core/C/engine/engine.c
+++ src/core/C/engine/engine.c
@@ -1,5 +1,7 @@
 #include "X3D_core.h"
 
+#include "engine/X3D_enginestate.h"
+
 X3D_EngineState x3d_global_state;
 
 X3D_ENGINE_STATE_DEFINITION;
--- src/headers/util/X3D_util.h
+++ src/headers/util/X3D_util.h
@@ -2,12 +2,12 @@
 
 #include "X3D_config.h"
 
-//#include "X3D_platform.h"
+// Common includes
 #include "common/X3D_int.h"
 #include "common/X3D_fix.h"
 
 // Interface include files
-#include "memory/X3D_memory.h"
+#include "init/X3D_init.h"
 
 #include "X3D_interface_implementation.h"
 
