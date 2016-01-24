10a93cb45479c4b36a7e56421fb36af021f7c645
Michael Wilder <mwilder.w@gmail.com>
2016-01-23 21:17:51 -0500

    Documented X3D_slaballocator.h

--- src/headers/memory/X3D_slaballocator.h
+++ src/headers/memory/X3D_slaballocator.h
@@ -19,18 +19,27 @@
 #include "X3D_assert.h"
 #include "memory/X3D_stack.h"
 
+//
 typedef struct X3D_SlabBlock {
   struct X3D_SlabBlock* next;
 } X3D_SlabBlock;
 
+// A freelist with all the blocks of the same size
 typedef struct X3D_Slab {
   uint16 size;
   X3D_SlabBlock* head;
   X3D_SlabBlock* tail;
 } X3D_Slab;
 
+// Max number of distinct slab sizes in a slab allocator
 #define X3D_TOTAL_SLABS 32
 
+///////////////////////////////////////////////////////////////////////////////
+/// An allocator suitable for several small allocations (< 512 bytes). Blocks
+///   are created on demand from a memory pool and then recycled when they are
+///   freed. Note that one a block has been created from the pool, it can
+///   never change its size.
+///////////////////////////////////////////////////////////////////////////////
 typedef struct X3D_SlabAllocator {
   X3D_Stack stack;
   X3D_Slab slabs[X3D_TOTAL_SLABS];
