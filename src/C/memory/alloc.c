dde922b00154c6b1dd6097852e68d836240c1336
Michael Wilder <mwilder.w@gmail.com>
2016-01-23 21:24:58 -0500

    Documented alloc.c

--- src/C/memory/alloc.c
+++ src/C/memory/alloc.c
@@ -18,16 +18,38 @@
 #include "memory/X3D_slaballocator.h"
 #include "X3D_enginestate.h"
 
-/// @todo Document.
-
+///////////////////////////////////////////////////////////////////////////////
+/// Initializes the alloc manager with the given memory pool size.
+///
+/// @param mem_size - size of the memory pool to create
+///
+/// @return Nothing.
+///////////////////////////////////////////////////////////////////////////////
 void x3d_allocmanager_init(size_t mem_size) {
   x3d_slaballocator_init(&x3d_allocmanager_get()->alloc, mem_size);
 }
 
+///////////////////////////////////////////////////////////////////////////////
+/// Allocates a small block ( < 512 bytes) from the global engine slab
+///   allocator.
+///
+/// @param size - size of the block to allocate
+///
+/// @return A block of memory that is at least size bytes.
+///////////////////////////////////////////////////////////////////////////////
 void* x3d_slab_alloc(uint16 size) {
   return x3d_slaballocator_alloc(&x3d_allocmanager_get()->alloc, size);
 }
 
+///////////////////////////////////////////////////////////////////////////////
+/// Frees a block back to the global engine slab allocator.
+///
+/// @param mem  - block to free
+///
+/// @return Nothing.
+/// @note No checks are performed to make sure that mem was actually alloced
+///   from alloc - be careful!
+///////////////////////////////////////////////////////////////////////////////
 void x3d_slab_free(void* mem) {
   x3d_slaballocator_free(&x3d_allocmanager_get()->alloc, mem);
 }
